4c37e6e25744973a1a1f3ac67983cc1310ea899b
AdrienTurmo <adrien.turmo@gmail.com>
2021-12-13 23:45:05 +0100

    Resolve Day 8

---
+++ src/components/Day8/Day8.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Day8.module.scss';
+
+//interface Props {}
+
+export const Day8: React.FC /*<Props>*/ = () => (
+  <div className={styles.Day8} data-testid="Day8">
+    Day8 component
+  </div>
+);
