ea3d1fc35f43445a589f83ac4f1bd4632d22bbdc
AdrienTurmo <adrien.turmo@gmail.com>
2021-12-13 23:46:05 +0100

    Resolve Day 9

---
+++ src/components/Day9/Day9.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Day9.module.scss';
+
+//interface Props {}
+
+export const Day9: React.FC /*<Props>*/ = () => (
+  <div className={styles.Day9} data-testid="Day9">
+    Day9 component
+  </div>
+);
