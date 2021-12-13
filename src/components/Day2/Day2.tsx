6b2d84230192ccbeb490fcbcd437e3838a615814
AdrienTurmo <adrien.turmo@gmail.com>
2021-12-14 00:41:35 +0100

    Resolve Day 2

---
+++ src/components/Day2/Day2.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Day2.module.scss';
+
+//interface Props {}
+
+export const Day2: React.FC /*<Props>*/ = () => (
+  <div className={styles.Day2} data-testid="Day2">
+    Day2 component
+  </div>
+);
