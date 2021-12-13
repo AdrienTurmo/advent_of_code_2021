1bec0fa28729e7db6c960156b4470db5b9c6d44f
AdrienTurmo <adrien.turmo@gmail.com>
2021-12-13 23:55:58 +0100

    Resolve Day 10

---
+++ src/components/Day10/Day10.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Day10.module.scss';
+
+//interface Props {}
+
+export const Day10: React.FC /*<Props>*/ = () => (
+  <div className={styles.Day10} data-testid="Day10">
+    Day10 component
+  </div>
+);
