6c2ae99796f06469f870214c79221c11947fb53d
AdrienTurmo <adrien.turmo@gmail.com>
2021-12-13 23:44:25 +0100

    Resolve Day 7

---
+++ src/components/Day7/Day7.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Day7.module.scss';
+
+//interface Props {}
+
+export const Day7: React.FC /*<Props>*/ = () => (
+  <div className={styles.Day7} data-testid="Day7">
+    Day7 component
+  </div>
+);
